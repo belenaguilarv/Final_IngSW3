169f66b1b455e8c8c1964492ba4cd4d75d771094
belenaguilarv <belenaguiilarv@gmail.com>
2024-02-19 16:52:41 -0300

    GET unit test added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 
+
 // GET all the products from the list
 app.get('/products', async (req, res) => {
     try {
---
+++ backend/test.js
@@ -0,0 +1,54 @@
+import request from "supertest";
+import app from "./index.js";
+import { expect } from "chai";
+import { db } from "./db.js";
+import sinon from "sinon";
+
+describe("/GET list", async () => {
+
+    it("should respond with a valid array of products", async () => {
+        
+        const mockProducts = [
+            { id: 1, product: "Product 1", quantity: 10 }, 
+            { id: 2, product: "Product 2", quantity: 20 }
+        ];
+
+        const queryStub = sinon.stub(db, "query");
+        queryStub.withArgs("SELECT * FROM list").resolves([mockProducts]);
+
+        const res = await request(app).get("/products");
+
+        expect(res.body).to.deep.equal(mockProducts);
+        queryStub.restore();
+    });
+
+});  
+
+/*
+describe("/POST products", () => {
+
+    it("should insert a new product into the list", async () => {
+
+        const newProduct = {
+            product: "New Product",
+            quantity: 15
+        };
+
+        const insertStub = sinon.stub(db, "query");
+
+        insertStub.withArgs("INSERT INTO list (product, quantity) VALUES (?, ?)",
+            [newProduct.product, newProduct.quantity]
+        ).resolves({ insertId: 10000 });
+
+        const res = await request(app).post("/products").send(newProduct);
+
+        expect(res.status).to.equal(201);
+  //      expect(res.body).to.deep.equal({ id: 10000, ...newProduct });
+        insertStub.restore();
+    });
+});
+*/
+    // Finaliza el proceso después de las pruebas
+    after(function () {
+        process.exit(0); 
+    });

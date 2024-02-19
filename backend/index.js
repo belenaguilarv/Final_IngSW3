c7b23f8cfcf5defff67ce4d03acd521045a2ac94
belenaguilarv <belenaguiilarv@gmail.com>
2024-02-19 11:15:47 -0300

    /GET /POST /DELETE logic added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import {db} from './db.js'
 
-
 const app = express();
 
 app.use(express.json());
-
-
-
-
 
 
 // GET all the products from the list
@@ -22,13 +17,33 @@ app.get('/products', async (req, res) => {
 });
 
 // INSERT a product
-app.post('/products', (req, res) => {
-    res.send("Insert a product to buy");
+app.post('/products', async (req, res) => {
+    try {
+        const {product, quantity} = req.body
+        const [rows] = await db.query(
+            'INSERT INTO list (product, quantity) VALUES (?, ?)', [product, quantity]
+        );
+        res.status(201).json({ id: rows.insertId, product, quantity });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
 });
 
 // DELETE a product from the list
-app.delete('/products/:id', (req, res) => {
-    res.send('Delete product from the list');
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [rows] = await db.query("DELETE FROM list WHERE id = ?", [id]);
+
+        if (rows.affectedRows <= 0) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.sendStatus(204);
+      
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });        
+    }
 });
 
 
